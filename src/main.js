dbc881a5c054edcd0eaf43682b54d4400fa32dd2
312173544@qq.com <312173544@qq.com>
2021-08-16 11:08:21 +0800

    添加视频播放

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+import VideoPlayer from 'vue-video-player'
+import 'vue-video-player/src/custom-theme.css'
+import 'video.js/dist/video-js.css'
+Vue.use(VideoPlayer)
 
 Vue.prototype.$deepCopy = deepCopy
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Test from '../views/Test.vue'
 import Excel from '../views/Excel.vue'
+import Movies from '../views/Movies.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { 
     path: '/', 
-    redirect: '/test'
+    redirect: '/Movies'
  },
   {
     path: '/test',
@@ -19,6 +20,11 @@ const routes = [
     path: '/excel',
     name: 'Excel',
     component: Excel
+  },
+  {
+    path: '/movies',
+    name: 'Movies',
+    component: Movies
   },
 ]
 
